44604a3c18d6ce249f88b7058c4c9aa8dc5a92bd
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2024-11-22 20:56:54 +0700

    feat: add skybox background

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,17 @@
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls, useTexture } from "@react-three/drei";
+import * as THREE from "three";
 
 export const Experience = () => {
+  const map = useTexture('textures/anime_art_style_a_water_based_pokemon_like_environ.jpg')
+
   return (
     <>
+      <ambientLight intensity={0.5} />
+      <Environment preset="sunset" />
       <OrbitControls />
       <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
+        <sphereGeometry args={[5, 32, 32]} />
+        <meshStandardMaterial map={map} side={THREE.BackSide} />
       </mesh>
     </>
   );
