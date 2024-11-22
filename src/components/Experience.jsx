7e8de2fdab0e5a65869903b13b71b1ddcfc5226e
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2024-11-22 22:04:39 +0700

    feat(models): add models

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,6 @@
 import { Environment, OrbitControls, useTexture } from "@react-three/drei";
 import * as THREE from "three";
+import Fish from "./Fish"
 
 export const Experience = () => {
   const map = useTexture('textures/anime_art_style_a_water_based_pokemon_like_environ.jpg')
@@ -9,6 +10,7 @@ export const Experience = () => {
       <ambientLight intensity={0.5} />
       <Environment preset="sunset" />
       <OrbitControls />
+      <Fish scale={0.6} position-y={-1} />
       <mesh>
         <sphereGeometry args={[10, 32, 32]} />
         <meshStandardMaterial map={map} side={THREE.BackSide} />
