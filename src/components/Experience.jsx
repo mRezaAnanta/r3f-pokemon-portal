1194838f53576c76d07e238000f07daa90040382
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2024-11-25 10:35:56 +0700

    feat: add some text

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import { Environment, MeshPortalMaterial, OrbitControls, RoundedBox, useTexture } from "@react-three/drei";
+import { Environment, MeshPortalMaterial, OrbitControls, RoundedBox, useTexture, Text } from "@react-three/drei";
 import * as THREE from "three";
 import Fish from "./Fish"
 import Dragon from "./Dragon"
@@ -11,22 +11,45 @@ export const Experience = () => {
       <ambientLight intensity={0.5} />
       <Environment preset="sunset" />
       <OrbitControls />
-      <MonsterStage texture={'textures/anime_art_style_a_water_based_pokemon_like_environ.jpg'}>
+      <MonsterStage
+        texture={'textures/anime_art_style_a_water_based_pokemon_like_environ.jpg'}
+        name={"Fish"}
+        color={"#38adcf"}
+      >
         <Fish scale={0.6} position-y={-1} />
       </MonsterStage>
-      <MonsterStage texture={'textures/anime_art_style_lava_world.jpg'} position-x={-2.5} rotation-y={Math.PI / 8}>
+      <MonsterStage
+        texture={'textures/anime_art_style_lava_world.jpg'}
+        name={"Dragon"}
+        color={"#df8d52"}
+        position-x={-2.5}
+        rotation-y={Math.PI / 8}>
         <Dragon scale={0.6} position-y={-1} />
       </MonsterStage>
-      <MonsterStage texture={'textures/anime_art_style_cactus_forest.jpg'} position-x={2.5} rotation-y={-Math.PI / 8}>
+      <MonsterStage
+        texture={'textures/anime_art_style_cactus_forest.jpg'}
+        name={"Cactus"}
+        color={"#739d3c"}
+        position-x={2.5}
+        rotation-y={-Math.PI / 8}>
         <Cactus scale={0.6} position-y={-1} />
       </MonsterStage>
     </>
   );
 };
 
-const MonsterStage = ({ children, texture, ...props }) => {
+const MonsterStage = ({ children, texture, name, color, ...props }) => {
   const map = useTexture(texture)
   return <group {...props}>
+    <Text
+      font="fonts/Caprasimo-Regular.ttf"
+      fontSize={0.3}
+      position={[0, -1.4, 0.05]}
+      anchorY={"bottom"}
+    >
+      {name}
+      <meshBasicMaterial color={color} toneMapped={false} />
+    </Text>
     <RoundedBox args={[2, 3, 0.1]}>
       <MeshPortalMaterial side={THREE.DoubleSide}>
         <ambientLight intensity={1} />
